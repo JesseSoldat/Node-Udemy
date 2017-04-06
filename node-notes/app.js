f464a834cc05a816415a43b04c0bac35d5348e22
jesse <jsoldat@hotmail.com>
2017-04-06 09:48:11 +0700

    added package.json

--- node-notes/app.js
+++ node-notes/app.js
@@ -1,12 +1,18 @@
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
 let user = os.userInfo();
 
 let greeting = "Hello to all my friends and enemies";
 
-console.log(user);
-fs.appendFileSync('user-info.txt', `Hello ${user.username}!`);
+let res = notes.addNote();
+// console.log(res);
+
+console.log(notes.add(5,5));
+
+// console.log(user);
+// fs.appendFileSync('user-info.txt', `Hello ${user.username}! You are ${notes.age} years old.`);
 
 // fs.appendFile('greetings.txt', greeting, function(err){
 // 	if(err) {
---
+++ node-notes/notes.js
@@ -0,0 +1,12 @@
+// console.log('notes is starting');
+module.exports.add = (a,b) => {
+	return a + b;
+};
+
+module.exports.addNote = () => {
+	// console.log('addNote');
+	return 'New Note';
+};
+
+module.exports.age = 36;
+// console.log(module);
